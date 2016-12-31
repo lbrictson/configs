82a9366ea125f4a54bf068ca8a020b520fe5addd
lbrictson <lucas.brictson@guaranteedrate.com>
2016-12-31 12:36:52 -0600

    Create app.py

---
+++ flask/api/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+version = 0.0.1
+
+app = Flask(__name__)
+api = Api(app)
+
+class version(Resource):
+    def get(self):
+        return {'version': version}
+
+
+class heartbeat(Resource):
+    def get(self):
+        return {'heartbeat': 'OK'}
+
+
+api.add_resource(heartbeat, '/heartbeat')
+api.add_resource(version, '/version')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
